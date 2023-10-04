7d165e05157a1bcd2b80a054c283c5410887238d
a-sumo <armandsumo@gmail.com>
2023-10-04 23:40:22 +0200

    add webgpu device

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "stb_image_write.h"
 #include <iostream>
 #include <mutex>
+#include "webgpu-utils.h"
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu.h>
 #include <glfw3webgpu.h>
@@ -25,56 +26,6 @@ uint8_t **readState = nullptr;  // Used by audio_callback
 uint8_t **writeState = nullptr; // Updated by automaton
 
 std::mutex bufferSwapMutex; // Used to protect buffer swap operations
-
-/**
- * Utility function to get a WebGPU adapter, so that
- *     WGPUAdapter adapter = requestAdapter(options);
- * is roughly equivalent to
- *     const adapter = await navigator.gpu.requestAdapter(options);
- */
-WGPUAdapter requestAdapter(WGPUInstance instance, WGPURequestAdapterOptions const * options) {
-	// A simple structure holding the local information shared with the
-	// onAdapterRequestEnded callback.
-	struct UserData {
-		WGPUAdapter adapter = nullptr;
-		bool requestEnded = false;
-	};
-	UserData userData;
-
-	// Callback called by wgpuInstanceRequestAdapter when the request returns
-	// This is a C++ lambda function, but could be any function defined in the
-	// global scope. It must be non-capturing (the brackets [] are empty) so
-	// that it behaves like a regular C function pointer, which is what
-	// wgpuInstanceRequestAdapter expects (WebGPU being a C API). The workaround
-	// is to convey what we want to capture through the pUserData pointer,
-	// provided as the last argument of wgpuInstanceRequestAdapter and received
-	// by the callback as its last argument.
-	auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const * message, void * pUserData) {
-		UserData& userData = *reinterpret_cast<UserData*>(pUserData);
-		if (status == WGPURequestAdapterStatus_Success) {
-			userData.adapter = adapter;
-		} else {
-			std::cout << "Could not get WebGPU adapter: " << message << std::endl;
-		}
-		userData.requestEnded = true;
-	};
-
-	// Call to the WebGPU request adapter procedure
-	wgpuInstanceRequestAdapter(
-		instance /* equivalent of navigator.gpu */,
-		options,
-		onAdapterRequestEnded,
-		(void*)&userData
-	);
-
-	// In theory we should wait until onAdapterReady has been called, which
-	// could take some time (what the 'await' keyword does in the JavaScript
-	// code). In practice, we know that when the wgpuInstanceRequestAdapter()
-	// function returns its callback has been called.
-	assert(userData.requestEnded);
-
-	return userData.adapter;
-}
 
 int main(int, char **)
 {
@@ -119,6 +70,29 @@ int main(int, char **)
     WGPUAdapter adapter = requestAdapter(instance, &adapterOpts);
 
     std::cout << "Got adapter: " << adapter << std::endl;
+
+    std::cout << "Requesting device..." << std::endl;
+
+    WGPUDeviceDescriptor deviceDesc = {};
+    deviceDesc.nextInChain = nullptr;
+    deviceDesc.label = "My Device";       // anything works here, that's your call
+    deviceDesc.requiredFeaturesCount = 0; // we do not require any specific feature
+    deviceDesc.requiredLimits = nullptr;  // we do not require any specific limit
+    deviceDesc.defaultQueue.nextInChain = nullptr;
+    deviceDesc.defaultQueue.label = "The default queue";
+    WGPUDevice device = requestDevice(adapter, &deviceDesc);
+
+    std::cout << "Got device: " << device << std::endl;
+    // Add a callback that gets executed upon errors in our use of the device
+    auto onDeviceError = [](WGPUErrorType type, char const *message, void * /* pUserData */)
+    {
+        std::cout << "Uncaptured device error: type " << type;
+        if (message)
+            std::cout << " (" << message << ")";
+        std::cout << std::endl;
+    };
+    
+    wgpuDeviceSetUncapturedErrorCallback(device, onDeviceError, nullptr /* pUserData */);
 
     uint8_t **current_state = (uint8_t **)malloc(HEIGHT * sizeof(uint8_t *));
     uint8_t **next_state = (uint8_t **)malloc(HEIGHT * sizeof(uint8_t *));
@@ -271,6 +245,7 @@ int main(int, char **)
     }
 
     // Don't forget to release the adapter
+    wgpuDeviceRelease(device);
     wgpuSurfaceRelease(surface);
     wgpuAdapterRelease(adapter);
     wgpuInstanceRelease(instance);
